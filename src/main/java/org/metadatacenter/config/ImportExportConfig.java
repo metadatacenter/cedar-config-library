057b0a626aca496418c20163445bbc0ff6708d03
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-06-24 10:04:45 -0700

    directory for export conversion

--- src/main/java/org/metadatacenter/config/ImportExportConfig.java
+++ src/main/java/org/metadatacenter/config/ImportExportConfig.java
@@ -4,10 +4,16 @@ public class ImportExportConfig {
 
   private String exportDir;
 
+  private String exportConvertedDir;
+
   private String importDir;
 
   public String getExportDir() {
     return exportDir;
+  }
+
+  public String getExportConvertedDir() {
+    return exportConvertedDir;
   }
 
   public String getImportDir() {
